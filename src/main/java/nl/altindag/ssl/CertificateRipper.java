2dce85def3c5c8b363c084448cf5d79f625f2da7
Hakan Altindag <hakangoudberg@hotmail.com>
2021-05-15 15:25:34 +0200

    Simplified functions

--- src/main/java/nl/altindag/ssl/CertificateRipper.java
+++ src/main/java/nl/altindag/ssl/CertificateRipper.java
@@ -8,16 +8,13 @@ import java.security.cert.Certificate;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.function.BiConsumer;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class CertificateRipper {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CertificateRipper.class);
-
-    private static final String NEW_LINE = System.lineSeparator();
-    private static final String CERTIFICATE_DELIMITER = NEW_LINE + NEW_LINE + "========== NEXT CERTIFICATE FOR %s ==========" + NEW_LINE + NEW_LINE;
+    private static final String CERTIFICATE_DELIMITER = "%n%n========== NEXT CERTIFICATE FOR %s ==========%n%n";
 
     public static void main(String[] applicationArguments) {
         boolean shouldBePemFormatted = Arrays.stream(applicationArguments)
@@ -25,31 +22,30 @@ public class CertificateRipper {
                 .anyMatch(argument -> argument.contains("format=pem"));
 
         if (shouldBePemFormatted) {
-            extractAndConsumeCertificates(applicationArguments, CertificateUtils::getCertificateAsPem, (key, value) ->
-                    LOGGER.info("Url = {}\n\n{}\n\n",
-                            key,
-                            String.join(String.format(CERTIFICATE_DELIMITER, key), value)));
+            extractCertificates(applicationArguments, CertificateUtils::getCertificateAsPem)
+                    .forEach((String url, List<String> certificate) ->
+                            LOGGER.info("Url = {}\n\n{}\n\n",
+                                    url,
+                                    String.join(String.format(CERTIFICATE_DELIMITER, url), certificate)));
         } else {
-            extractAndConsumeCertificates(applicationArguments, CertificateUtils::getCertificate, (key, value) ->
-                    LOGGER.info("Url = {}\n\n{}\n\n",
-                            key,
-                            value.stream()
-                                    .map(Certificate::toString)
-                                    .collect(Collectors.joining(String.format(CERTIFICATE_DELIMITER, key)))
-                            )
-            );
+            extractCertificates(applicationArguments, CertificateUtils::getCertificate)
+                    .forEach((String url, List<Certificate> certificates) ->
+                            LOGGER.info("Url = {}\n\n{}\n\n",
+                                    url,
+                                    certificates.stream()
+                                            .map(Certificate::toString)
+                                            .collect(Collectors.joining(String.format(CERTIFICATE_DELIMITER, url)))
+                            ));
         }
     }
 
-    private static <T> void extractAndConsumeCertificates(String[] applicationArguments,
-                                                          Function<List<String>, Map<String, List<T>>> urlsToCertificates,
-                                                          BiConsumer<String, List<T>> certificateConsumer) {
-        Arrays.stream(applicationArguments)
+    private static <T> Map<String, List<T>> extractCertificates(String[] applicationArguments,
+                                                                Function<List<String>, Map<String, List<T>>> urlsToCertificates) {
+        return Arrays.stream(applicationArguments)
                 .map(String::toLowerCase)
                 .filter(argument -> argument.contains("url="))
                 .map(parameter -> parameter.split("url=")[1])
-                .collect(Collectors.collectingAndThen(Collectors.toList(), urlsToCertificates))
-                .forEach(certificateConsumer);
+                .collect(Collectors.collectingAndThen(Collectors.toList(), urlsToCertificates));
     }
 
 }
