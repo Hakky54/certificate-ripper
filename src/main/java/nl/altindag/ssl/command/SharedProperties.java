36fa1842cee55f00a70f58dd5841d106e46ee574
Hakan Altindag <hakangoudberg@hotmail.com>
2021-05-18 23:57:44 +0200

    Simplified extracting certificates

--- src/main/java/nl/altindag/ssl/command/SharedProperties.java
+++ src/main/java/nl/altindag/ssl/command/SharedProperties.java
@@ -4,33 +4,24 @@ import nl.altindag.ssl.util.CertificateUtils;
 import picocli.CommandLine.Option;
 
 import java.security.cert.Certificate;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
+@SuppressWarnings("unused")
 class SharedProperties {
 
-    private final Map<String, List<Certificate>> urlsToCertificates = new HashMap<>();
-    private final Set<String> urls = new HashSet<>();
+    @Option(names = {"-u", "--url"}, description = "Url of the target server to extract the certificates", required = true)
+    private String[] urls;
 
+    private Map<String, List<Certificate>> urlsToCertificates;
     private boolean isExtracted = false;
 
     public Map<String, List<Certificate>> getUrlsToCertificates() {
         if (!isExtracted) {
-            Map<String, List<Certificate>> certificate = CertificateUtils.getCertificate(new ArrayList<>(urls));
-            urlsToCertificates.putAll(certificate);
+            urlsToCertificates = CertificateUtils.getCertificate(urls);
             isExtracted = true;
         }
         return urlsToCertificates;
-    }
-
-    @Option(names = {"-u", "--url"}, description = "Url of the target server to extract the certificates", required = true)
-    public void urls(String[] urls) {
-        this.urls.addAll(Arrays.asList(urls));
     }
 
 }
