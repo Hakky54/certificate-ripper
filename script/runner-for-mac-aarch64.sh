c14ef1c3bb73bca9b57444f565b1e26395f293af
Hakky54 <hakangoudberg@hotmail.com>
2022-12-16 23:30:06 +0100

    Added script for mac with apple chip

--- script/runner-for-mac-aarch64.sh
+++ script/runner-for-mac-aarch64.sh
@@ -1,1 +1,14 @@
 #!/usr/bin/env bash
+
+getBinaries() {
+  curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-darwin-aarch64-22.3.0.tar.gz | tar -xzf - -C bin/graalvm --strip-components=1
+  curl -sL https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz | tar -xzf - -C bin/maven --strip-components=1
+}
+
+mkdir -p bin/graalvm
+mkdir -p bin/maven
+getBinaries
+JAVA_HOME=`pwd`"/bin/graalvm/Contents/Home"
+bin/graalvm/Contents/Home/bin/gu install native-image
+bin/maven/bin/mvn clean install -Pnative-image
+cp target/crip bin/crip
