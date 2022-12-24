42fb4e0c903facbc205ddfb6b5948c68c3c13995
Hakky54 <hakangoudberg@hotmail.com>
2022-12-24 19:28:03 +0100

    Bumped to version 2

--- src/main/java/nl/altindag/crip/command/CertificateRipper.java
+++ src/main/java/nl/altindag/crip/command/CertificateRipper.java
@@ -20,7 +20,7 @@ import picocli.CommandLine.Command;
 
 @Command(
         name = "crip",
-        version = "crip v1.0.0",
+        version = "crip v2.0.0",
         description = "CLI tool to extracts server certificates",
         subcommands = {
                 PrintCommand.class,
