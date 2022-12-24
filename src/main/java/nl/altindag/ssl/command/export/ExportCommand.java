f62219f272b77ecbbbc00b3c0f6bde1551ed5d52
Hakky54 <hakangoudberg@hotmail.com>
2022-12-24 02:12:54 +0100

    Updated documentation

--- src/main/java/nl/altindag/ssl/command/export/ExportCommand.java
+++ src/main/java/nl/altindag/ssl/command/export/ExportCommand.java
@@ -2,11 +2,13 @@ package nl.altindag.ssl.command.export;
 
 import picocli.CommandLine.Command;
 
-@Command(name = "export", subcommands = {
-        Pkcs12ExportCommand.class,
-        DerExportCommand.class,
-        PemExportCommand.class
-})
+@Command(name = "export",
+        description = "Export the extracted certificate to a provided output type",
+        subcommands = {
+                Pkcs12ExportCommand.class,
+                DerExportCommand.class,
+                PemExportCommand.class
+        })
 public class ExportCommand {
 
 }
