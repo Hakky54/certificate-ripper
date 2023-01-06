fd6c303305c0164df4c43e047393bbe1f47fbf5e
Hakky54 <hakangoudberg@hotmail.com>
2023-01-06 14:25:35 +0100

    Added new parameter to graalvm configuration and bumped maven version

--- script/build-native-executable-for-linux-amd64.sh
+++ script/build-native-executable-for-linux-amd64.sh
@@ -2,7 +2,7 @@
 
 getBinaries() {
   curl -sL https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-22.3.0/graalvm-ce-java17-linux-amd64-22.3.0.tar.gz | tar -xzf - -C bin/graalvm --strip-components=1
-  curl -sL https://dlcdn.apache.org/maven/maven-3/3.8.6/binaries/apache-maven-3.8.6-bin.tar.gz | tar -xzf - -C bin/maven --strip-components=1
+  curl -sL https://dlcdn.apache.org/maven/maven-3/3.8.7/binaries/apache-maven-3.8.7-bin.tar.gz | tar -xzf - -C bin/maven --strip-components=1
 }
 
 sudo apt-get update && sudo apt-get install build-essential libz-dev zlib1g-dev -y
