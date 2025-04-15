7767a38ccbc6407509197e4de1d8c91c63d2b526
Hakky54 <hakangoudberg@hotmail.com>
2025-04-16 00:33:19 +0200

    Added support for websockets

---
+++ src/main/java/nl/altindag/crip/client/websocket/WebSocketListener.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2021 Thunderberry.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.altindag.crip.client.websocket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.http.WebSocket;
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+public final class WebSocketListener implements WebSocket.Listener {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(WebSocketListener.class);
+
+    private final CountDownLatch latch;
+
+    public WebSocketListener(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    @Override
+    public void onOpen(WebSocket webSocket) {
+        LOGGER.debug("WebSocket opened");
+        WebSocket.Listener.super.onOpen(webSocket);
+    }
+
+    @Override
+    public CompletionStage<?> onText(WebSocket webSocket, CharSequence data, boolean last) {
+        LOGGER.debug("WebSocket text received: {}", data);
+        latch.countDown();
+        return WebSocket.Listener.super.onText(webSocket, data, last);
+    }
+
+    @Override
+    public void onError(WebSocket webSocket, Throwable error) {
+        LOGGER.debug("WebSocket error", error);
+        WebSocket.Listener.super.onError(webSocket, error);
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public void waitForResponse(Duration timeout) {
+        try {
+            latch.await(timeout.toMillis(), TimeUnit.MILLISECONDS);
+        } catch (InterruptedException e) {
+            // skip
+        }
+    }
+
+    @SuppressWarnings("ResultOfMethodCallIgnored")
+    public void waitForResponse() {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            // skip
+        }
+    }
+
+}
