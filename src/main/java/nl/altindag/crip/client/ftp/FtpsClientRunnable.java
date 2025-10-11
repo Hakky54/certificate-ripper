0eeb4319774c7295dcd465f817f2c81de0f6e4cb
Hakky54 <hakangoudberg@hotmail.com>
2025-10-11 18:05:14 +0200

    Added support for FTP

---
+++ src/main/java/nl/altindag/crip/client/ftp/FtpsClientRunnable.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2021 Thunderberry.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package nl.altindag.crip.client.ftp;
+
+import nl.altindag.ssl.model.ClientConfig;
+import nl.altindag.ssl.util.ClientRunnable;
+import org.apache.commons.net.ftp.FTPSClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.Duration;
+
+public class FtpsClientRunnable implements ClientRunnable {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(FtpsClientRunnable.class);
+
+    @Override
+    public void run(ClientConfig clientConfig, URI uri) {
+        FTPSClient client = new FTPSClient(clientConfig.getSslFactory().getSslContext());
+        clientConfig.getProxy().ifPresent(client::setProxy);
+        clientConfig.getTimeout()
+                .map(Duration::toMillis)
+                .map(Math::toIntExact)
+                .ifPresent(timeout -> {
+                    client.setDefaultTimeout(timeout);
+                    client.setConnectTimeout(timeout);
+                });
+
+        try {
+            client.connect(uri.getHost(), uri.getPort());
+        } catch (IOException e) {
+            LOGGER.debug("Could not connect to {}:{}", uri.getHost(), uri.getPort(), e);
+        }
+    }
+
+}
