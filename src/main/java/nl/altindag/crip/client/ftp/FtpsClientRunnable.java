1785f8f8b70356e78237bcbe58dfdd78c72948f1
Hakky54 <hakangoudberg@hotmail.com>
2025-10-12 21:56:34 +0200

    Properly close ftp client

--- src/main/java/nl/altindag/crip/client/ftp/FtpsClientRunnable.java
+++ src/main/java/nl/altindag/crip/client/ftp/FtpsClientRunnable.java
@@ -17,6 +17,7 @@ package nl.altindag.crip.client.ftp;
 
 import nl.altindag.ssl.model.ClientConfig;
 import nl.altindag.ssl.util.ClientRunnable;
+import org.apache.commons.net.ftp.FTPReply;
 import org.apache.commons.net.ftp.FTPSClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -43,8 +44,18 @@ public class FtpsClientRunnable implements ClientRunnable {
 
         try {
             client.connect(uri.getHost(), uri.getPort());
+            int replyCode = client.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(replyCode)) {
+                LOGGER.error("FTP server {} refused connection, reply code: {}", uri.getHost(), replyCode);
+            }
         } catch (IOException e) {
             LOGGER.debug("Could not connect to {}:{}", uri.getHost(), uri.getPort(), e);
+        } finally {
+            try {
+                client.disconnect();
+            } catch (IOException e) {
+                LOGGER.debug("Could not disconnect from {}:{}", uri.getHost(), uri.getPort(), e);
+            }
         }
     }
 
